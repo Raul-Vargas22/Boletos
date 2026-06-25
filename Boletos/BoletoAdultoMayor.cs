6184247f280193d9a59a3bf03e1c48d5bc0a8bae
Sangreazul_misti <joseluisfayad1825@gmail.com>
2026-06-25 09:08:52 -0600

    Agrege la parte del boleto de adulto mayor :d

---
+++ Boletos/BoletoAdultoMayor.cs
@@ -0,0 +1,20 @@
+﻿namespace Boletos
+{
+    public class BoletoAdultoMayor : Boleto
+    {
+        public string NumCredencialInapam { get; set; }
+
+        public BoletoAdultoMayor(string numCredencialInapam)
+        {
+            NumCredencialInapam = numCredencialInapam;
+        }
+
+        public override double CalcularPrecioFinal()
+        {
+            double descuento = PrecioBase * 0.50;
+            double precioFinal = PrecioBase - descuento;
+
+            return precioFinal;
+        }
+    }
+}
