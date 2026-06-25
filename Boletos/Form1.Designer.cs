004a8984721c3edaedf7a973f94c390a61b0fe0a
Sangreazul_misti <joseluisfayad1825@gmail.com>
2026-06-25 13:13:03 -0600

    Interfaz y la base de calcular

--- Boletos/Form1.Designer.cs
+++ Boletos/Form1.Designer.cs
@@ -30,9 +30,6 @@
         {
             BtnCalcular = new Button();
             label2 = new Label();
-            RBtnNormal = new RadioButton();
-            RBtnEstudiante = new RadioButton();
-            RBtnInapam = new RadioButton();
             cant_boletos = new NumericUpDown();
             label1 = new Label();
             pre_boleto = new TextBox();
@@ -41,6 +38,7 @@
             label4 = new Label();
             txtTotal = new TextBox();
             label3 = new Label();
+            cmbTipoBoleto = new ComboBox();
             ((System.ComponentModel.ISupportInitialize)cant_boletos).BeginInit();
             SuspendLayout();
             // 
@@ -48,132 +46,120 @@
             // 
             BtnCalcular.BackColor = Color.Black;
             BtnCalcular.ForeColor = SystemColors.ControlLight;
-            BtnCalcular.Location = new Point(354, 344);
+            BtnCalcular.Location = new Point(283, 275);
+            BtnCalcular.Margin = new Padding(2);
             BtnCalcular.Name = "BtnCalcular";
-            BtnCalcular.Size = new Size(112, 34);
+            BtnCalcular.Size = new Size(90, 27);
             BtnCalcular.TabIndex = 0;
             BtnCalcular.Text = "Calcular";
             BtnCalcular.UseVisualStyleBackColor = false;
+            BtnCalcular.Click += BtnCalcular_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(12, 79);
+            label2.Location = new Point(10, 63);
+            label2.Margin = new Padding(2, 0, 2, 0);
             label2.Name = "label2";
-            label2.Size = new Size(142, 25);
+            label2.Size = new Size(119, 20);
             label2.TabIndex = 2;
             label2.Text = "Precio de boleto";
-            // 
-            // RBtnNormal
-            // 
-            RBtnNormal.AutoSize = true;
-            RBtnNormal.Location = new Point(12, 12);
-            RBtnNormal.Name = "RBtnNormal";
-            RBtnNormal.Size = new Size(152, 29);
-            RBtnNormal.TabIndex = 3;
-            RBtnNormal.TabStop = true;
-            RBtnNormal.Text = "Boleto Normal";
-            RBtnNormal.UseVisualStyleBackColor = true;
-            // 
-            // RBtnEstudiante
-            // 
-            RBtnEstudiante.AutoSize = true;
-            RBtnEstudiante.Location = new Point(170, 12);
-            RBtnEstudiante.Name = "RBtnEstudiante";
-            RBtnEstudiante.Size = new Size(200, 29);
-            RBtnEstudiante.TabIndex = 4;
-            RBtnEstudiante.TabStop = true;
-            RBtnEstudiante.Text = "Boleto de estudiante";
-            RBtnEstudiante.UseVisualStyleBackColor = true;
-            RBtnEstudiante.CheckedChanged += RBtnEstudiante_CheckedChanged;
-            // 
-            // RBtnInapam
-            // 
-            RBtnInapam.AutoSize = true;
-            RBtnInapam.Location = new Point(376, 12);
-            RBtnInapam.Name = "RBtnInapam";
-            RBtnInapam.Size = new Size(160, 29);
-            RBtnInapam.TabIndex = 5;
-            RBtnInapam.TabStop = true;
-            RBtnInapam.Text = "Boleto INAPAM";
-            RBtnInapam.UseVisualStyleBackColor = true;
             // 
             // cant_boletos
             // 
             cant_boletos.BackColor = Color.FromArgb(255, 255, 192);
-            cant_boletos.Location = new Point(19, 290);
+            cant_boletos.Location = new Point(15, 232);
+            cant_boletos.Margin = new Padding(2);
             cant_boletos.Name = "cant_boletos";
-            cant_boletos.Size = new Size(92, 31);
+            cant_boletos.Size = new Size(74, 27);
             cant_boletos.TabIndex = 6;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(19, 247);
+            label1.Location = new Point(15, 198);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(173, 25);
+            label1.Size = new Size(144, 20);
             label1.TabIndex = 7;
             label1.Text = "Cantidad de boletos";
             // 
             // pre_boleto
             // 
             pre_boleto.BackColor = Color.Peru;
-            pre_boleto.Location = new Point(19, 122);
+            pre_boleto.Location = new Point(15, 98);
+            pre_boleto.Margin = new Padding(2);
             pre_boleto.Name = "pre_boleto";
             pre_boleto.ReadOnly = true;
-            pre_boleto.Size = new Size(150, 31);
+            pre_boleto.Size = new Size(121, 27);
             pre_boleto.TabIndex = 8;
             pre_boleto.Text = "80";
             // 
             // txtDato
             // 
             txtDato.BackColor = Color.IndianRed;
-            txtDato.Location = new Point(19, 194);
+            txtDato.Location = new Point(15, 155);
+            txtDato.Margin = new Padding(2);
             txtDato.Name = "txtDato";
-            txtDato.Size = new Size(150, 31);
+            txtDato.Size = new Size(121, 27);
             txtDato.TabIndex = 10;
             // 
             // lbl_Dato
             // 
             lbl_Dato.AutoSize = true;
-            lbl_Dato.Location = new Point(19, 166);
+            lbl_Dato.Location = new Point(15, 133);
+            lbl_Dato.Margin = new Padding(2, 0, 2, 0);
             lbl_Dato.Name = "lbl_Dato";
-            lbl_Dato.Size = new Size(84, 25);
+            lbl_Dato.Size = new Size(71, 20);
             lbl_Dato.TabIndex = 9;
             lbl_Dato.Text = "Matricula";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(569, 79);
+            label4.Location = new Point(455, 63);
+            label4.Margin = new Padding(2, 0, 2, 0);
             label4.Name = "label4";
-            label4.Size = new Size(0, 25);
+            label4.Size = new Size(0, 20);
             label4.TabIndex = 12;
             // 
             // txtTotal
             // 
             txtTotal.BackColor = SystemColors.HotTrack;
             txtTotal.ForeColor = SystemColors.Window;
-            txtTotal.Location = new Point(569, 122);
+            txtTotal.Location = new Point(455, 98);
+            txtTotal.Margin = new Padding(2);
             txtTotal.Name = "txtTotal";
-            txtTotal.Size = new Size(150, 31);
+            txtTotal.Size = new Size(121, 27);
             txtTotal.TabIndex = 13;
+            txtTotal.TextChanged += txtTotal_TextChanged;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(586, 79);
+            label3.Location = new Point(469, 63);
+            label3.Margin = new Padding(2, 0, 2, 0);
             label3.Name = "label3";
-            label3.Size = new Size(114, 25);
+            label3.Size = new Size(97, 20);
             label3.TabIndex = 14;
             label3.Text = "Total a pagar";
             // 
+            // cmbTipoBoleto
+            // 
+            cmbTipoBoleto.FormattingEnabled = true;
+            cmbTipoBoleto.Location = new Point(10, 12);
+            cmbTipoBoleto.Name = "cmbTipoBoleto";
+            cmbTipoBoleto.Size = new Size(151, 28);
+            cmbTipoBoleto.TabIndex = 15;
+            cmbTipoBoleto.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.RosyBrown;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(640, 360);
+            Controls.Add(cmbTipoBoleto);
             Controls.Add(label3);
             Controls.Add(txtTotal);
             Controls.Add(label4);
@@ -182,13 +168,12 @@
             Controls.Add(pre_boleto);
             Controls.Add(label1);
             Controls.Add(cant_boletos);
-            Controls.Add(RBtnInapam);
-            Controls.Add(RBtnEstudiante);
-            Controls.Add(RBtnNormal);
             Controls.Add(label2);
             Controls.Add(BtnCalcular);
+            Margin = new Padding(2);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ((System.ComponentModel.ISupportInitialize)cant_boletos).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -198,9 +183,6 @@
 
         private Button BtnCalcular;
         private Label label2;
-        private RadioButton RBtnNormal;
-        private RadioButton RBtnEstudiante;
-        private RadioButton RBtnInapam;
         private NumericUpDown cant_boletos;
         private Label label1;
         private TextBox pre_boleto;
@@ -209,5 +191,6 @@
         private Label label4;
         private TextBox txtTotal;
         private Label label3;
+        private ComboBox cmbTipoBoleto;
     }
 }
--- Boletos/Form1.cs
+++ Boletos/Form1.cs
@@ -5,32 +5,75 @@ namespace Boletos
         public Form1()
         {
             InitializeComponent();
-        }
 
-        private void RBtnEstudiante_CheckedChanged(object sender, EventArgs e)
-        {
-            if (RBtnNormal.Checked)
-            {
-                lbl_Dato.Hide();
-                txtDato.Hide();
-            }
-            else if (RBtnEstudiante.Checked)
-            {
-                lbl_Dato.Show();
-                lbl_Dato.Text = "Matricula";
-                txtDato.Show();
-            }
-            else
-            {
-                lbl_Dato.Show();
-                lbl_Dato.Text = "Codigo INAPAM";
-                txtDato.Show();
-            }
+            cmbTipoBoleto.Items.Add("General");
+            cmbTipoBoleto.Items.Add("Estudiante");
+            cmbTipoBoleto.Items.Add("Adulto Mayor");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void txtTotal_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTipoBoleto.Text == "General")
+            {
+                lbl_Dato.Hide();
+                txtDato.Hide();
+            }
+            else if (cmbTipoBoleto.Text == "Estudiante")
+            {
+                lbl_Dato.Show();
+                lbl_Dato.Text = "Matrícula:";
+                txtDato.Show();
+            }
+            else if (cmbTipoBoleto.Text == "Adulto Mayor")
+            {
+                lbl_Dato.Show();
+                lbl_Dato.Text = "Código INAPAM:";
+                txtDato.Show();
+            }
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+            Boleto boleto = null;
+
+            if (cmbTipoBoleto.Text == "Estudiante")
+            {
+                // Aquí va BoletoEstudiante
+            }
+            else if (cmbTipoBoleto.Text == "Adulto Mayor")
+            {
+                // Aquí va BoletoAdultoMayor
+            }
+            else if (cmbTipoBoleto.Text == "General")
+            {
+                // Aquí va BoletoGeneral
+            }
+            else
+            {
+                MessageBox.Show("Selecciona un tipo de boleto.");
+                return;
+            }
+
+            if (boleto != null)
+            {
+                double total = boleto.CalcularPrecioFinal();
+                txtTotal.Text = total.ToString("0.00");
+            }
         }
     }
 }
