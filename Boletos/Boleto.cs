1d7caabd41ffd69dc4b110e1d0b4ca506e63bba2
Sangreazul_misti <joseluisfayad1825@gmail.com>
2026-06-25 12:40:27 -0600

    Clase Boleto .-.

---
+++ Boletos/Boleto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boletos
+{
+    public abstract class Boleto
+    {
+        public double PrecioBase { get; set; }
+
+        public Boleto()
+        {
+            PrecioBase = 80.00;
+        }
+
+        public abstract double CalcularPrecioFinal();
+    }
+}
+
